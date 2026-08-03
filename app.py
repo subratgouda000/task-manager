b0c92fe318d275c4856a1801dee62ded3959e266
Subrat Gouda <subratgouda467@gmail.com>
2026-08-03 11:29:11 +0530

    Fix: stop test data leaking into CI, ignore local db files

--- app.py
+++ app.py
@@ -37,8 +37,6 @@ class Task(db.Model):
         }
 
 
-with app.app_context():
-    db.create_all()
 
 
 @app.route("/")
@@ -108,4 +106,6 @@ def delete_task(task_id):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
